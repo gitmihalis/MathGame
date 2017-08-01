b61fac7fad402deb6f0d4d2ad545ef21ca0c7d74
gitmihalis <mihalisfthenos@icloud.com>
2017-08-01 00:10:38 +0000

    add mocks and a sleep to MathGame::Question.ask

--- question.rb
+++ question.rb
@@ -5,19 +5,31 @@ module MathGame
 			@rand1 = rand(1..20)
 			@rand2 = rand(1..20)
 			@operator = [:+, :-].sample
+			@mocks = [
+				"I think it's time you wen't back to school.",
+				"Don't quit your day job.",
+				"Was that a serious answer?",
+				"No... just no.",
+				"That answer made me physically ill.",
+				"You parents must be so proud...",
+				"You make others feel better about themselves..."
+			]
 		end
 
 		def ask(player)
-			puts "[->-> Question for #{player.name} <-<-]"
-			puts "What is #{@rand1} #{@operator} #{@rand2} ?"
+			puts "#{player.name}, what is #{@rand1} #{@operator} #{@rand2} ?"
 			player_answer = gets.chomp.to_i
-
+			puts "..."
+			sleep(2)
 			if player_answer == ( @rand1.send(@operator, @rand2) )
-				puts "Good job. #{player_answer} is correct!"
+				puts "Good job #{player.name}. Your math is on point! 👌"
 			else 
 				player.lose_life
-				puts "What were you thinking? #{player_answer} is wrong."
-			end			
+				puts "What were you thinking? #{player_answer} is so wrong 🤢"
+				puts @mocks.sample
+			end
+			sleep(2)
+			puts "- - -" * 8
 		end
 
 	end
